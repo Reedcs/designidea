8ea1f683e7b9fc7ac7b4a59b00c8f4e72f616303
reed stoltzfus <reedcs@comcast.net>
2019-01-29 15:25:54 -0700

    First Commit

---
+++ Design Idea/Assets/Scripts/ObjectSnapping.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSnapping : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SnapObject();
+	}
+
+    void SnapObject()
+    {
+        var currentPos = transform.position;
+        transform.position = new Vector3(Mathf.Round(currentPos.x),
+                                     Mathf.Round(currentPos.y),
+                                     Mathf.Round(currentPos.z));
+    } 
+}
